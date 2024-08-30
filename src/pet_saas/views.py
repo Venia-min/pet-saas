f1e62d9e554c5a71aecab05f78b16823af7a308f
venia-min <57226644+Venia-min@users.noreply.github.com>
2024-08-30 23:36:59 +0200

    First commit

---
+++ src/pet_saas/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+import pathlib
+from src.visits.models import PageVisit
+
+
+def home_page_view(request, *args, **kwargs):
+    qs = PageVisit.objects.all()
+    page_qs = PageVisit.objects.filter(path=request.path)
+    my_title = "My Page"
+    my_context = {
+        "page_title": my_title,
+        "page_visit_count": page_qs.count(),
+        "percent": (page_qs.count() * 100.0) / qs.count(),
+        "total_visit_count": qs.count(),
+    }
+    html_template = "home.html"
+    PageVisit.objects.cteate(path=request.path)
+    return render(request, html_template, my_context)
+
+
+def about_page_view(request, *args, **kwargs):
+    my_title = "My Page"
+    my_context = {
+        "page_title": my_title
+    }
+    html_template = "about.html"
+    return render(request, html_template, my_context)
+
+
