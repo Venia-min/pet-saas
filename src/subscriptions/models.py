8c95a40f83b188528763d20c41d4a289eaad3849
venia-min <57226644+Venia-min@users.noreply.github.com>
2024-09-11 00:28:20 +0200

    Add sync user subscription with user group

--- src/subscriptions/models.py
+++ src/subscriptions/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import Group, Permission
+from django.db.models.signals import post_save
 from django.conf import settings
 
 
@@ -10,6 +11,8 @@ SUBSCRIPTION_PERMISSIONS = [
             ("pro", "Pro Perm"),
             ("basic", "Basic Perm")
         ]
+
+ALLOW_CUSTOM_GROUPS = True
 
 
 class Subscription(models.Model):
@@ -39,3 +42,30 @@ class UserSubscription(models.Model):
         blank=True
     )
     active = models.BooleanField(default=True)
+
+
+def user_sub_post_save(sender, instance, *args, **kwargs):
+    user_sub_instance = instance
+    user = user_sub_instance.user
+    subscription_obj = user_sub_instance.subscription
+    groups_ids = []
+    if subscription_obj is not None:
+        groups = subscription_obj.groups.all()
+        groups_ids = groups.values_list('id', flat=True)
+    if not ALLOW_CUSTOM_GROUPS:
+        user.groups.set(groups_ids)
+    else:
+        subs_qs = Subscription.objects.filter(active=True)
+        if subscription_obj is not None:
+            subs_qs = subs_qs.exclude(id=subscription_obj.id)
+        subs_groups = subs_qs.values_list("groups__id", flat=True)
+        subs_groups_set = set(subs_groups)
+        # groups_ids = groups.values_list('id', flat=True)  # [1, 2, 3]
+        current_groups = user.groups.all().values_list('id', flat=True)
+        groups_ids_set = set(groups_ids)
+        current_groups_set = set(current_groups) - subs_groups_set
+        final_group_ids = list(groups_ids_set | current_groups_set)
+        user.groups.set(final_group_ids)
+
+
+post_save.connect(user_sub_post_save, sender=UserSubscription)
