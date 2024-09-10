365a624c9afe9da0aad9d3ea73fc0a6948d18330
venia-min <57226644+Venia-min@users.noreply.github.com>
2024-09-10 23:20:38 +0200

    Add UserSubscription model

--- src/subscriptions/models.py
+++ src/subscriptions/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import Group, Permission
+from django.conf import settings
 
+
+User = settings.AUTH_USER_MODEL  # "auth.User"
 
 SUBSCRIPTION_PERMISSIONS = [
             ("advanced", "Advanced Perm"), # subscriptions.advanced
@@ -11,7 +14,7 @@ SUBSCRIPTION_PERMISSIONS = [
 
 class Subscription(models.Model):
     name = models.CharField(max_length=120)
-    groups = models.ManyToManyField(Group) # one-to-one
+    groups = models.ManyToManyField(Group)  # one-to-one
     active = models.BooleanField(default=True)
     permissions = models.ManyToManyField(Permission,
     limit_choices_to={
@@ -20,5 +23,19 @@ class Subscription(models.Model):
         }
     )
 
+    def __str__(self):
+        return f"{self.name}"
+
     class Meta:
         permissions = SUBSCRIPTION_PERMISSIONS
+
+
+class UserSubscription(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    subscription = models.ForeignKey(
+        Subscription,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+    active = models.BooleanField(default=True)
