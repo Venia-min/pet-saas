b2bd6d788d2fbf4ed58517a39d69141bb558d0ca
venia-min <57226644+Venia-min@users.noreply.github.com>
2024-09-16 00:09:54 +0200

    Add Subscription price model, create price function

--- src/subscriptions/models.py
+++ src/subscriptions/models.py
@@ -3,13 +3,15 @@ from django.contrib.auth.models import Group, Permission
 from django.db.models.signals import post_save
 from django.conf import settings
 
-from helpers.billing import create_product
+import stripe
+
+from helpers.billing import create_product, create_price
 
 
 User = settings.AUTH_USER_MODEL  # "auth.User"
 
 SUBSCRIPTION_PERMISSIONS = [
-            ("advanced", "Advanced Perm"), # subscriptions.advanced
+            ("advanced", "Advanced Perm"),  # subscriptions.advanced
             ("pro", "Pro Perm"),
             ("basic", "Basic Perm")
         ]
@@ -21,10 +23,11 @@ class Subscription(models.Model):
     name = models.CharField(max_length=120)
     active = models.BooleanField(default=True)
     groups = models.ManyToManyField(Group)  # one-to-one
-    permissions = models.ManyToManyField(Permission,
-    limit_choices_to={
-        "content_type__app_label": "subscriptions",
-        "codename__in": [x[0] for x in SUBSCRIPTION_PERMISSIONS]
+    permissions = models.ManyToManyField(
+        Permission,
+        limit_choices_to={
+            "content_type__app_label": "subscriptions",
+            "codename__in": [x[0] for x in SUBSCRIPTION_PERMISSIONS]
         }
     )
     stripe_id = models.CharField(max_length=120, null=True, blank=True)
@@ -39,7 +42,53 @@ class Subscription(models.Model):
         if not self.stripe_id:
             stripe_id = create_product(
                 name=self.name,
-                metadata={"subscription_plan_id": self.id
+                metadata={"subscription_plan_id": self.id},
+                raw=False
+            )
+            self.stripe_id = stripe_id
+        super().save(*args, **kwargs)
+
+
+class SubscriptionPrice(models.Model):
+
+    class IntervalChoices(models.TextChoices):
+        MONTHLY = "month", "Monthly"
+        YEARLY = "year", "Yearly"
+
+    subscription = models.ForeignKey(Subscription, on_delete=models.SET_NULL, null=True)
+    stripe_id = models.CharField(max_length=120, null=True, blank=True)
+    interval = models.CharField(max_length=120,
+                                default=IntervalChoices.MONTHLY,
+                                choices=IntervalChoices.choices
+                                )
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=99.99)
+
+    @property
+    def stripe_currency(self):
+        return "usd"
+
+    @property
+    def stripe_price(self):
+        """
+        remove decimal places
+        """
+        return self.price * 100
+
+    @property
+    def product_stripe_id(self):
+        if not self.subscription:
+            return None
+        return self.subscription.stripe_id
+
+    def save(self, *args, **kwargs):
+        if not self.stripe_id and self.product_stripe_id is not None:
+            stripe_id = helpers.billing.create_price(
+                currency=self.stripe_currency,
+                unit_amount=self.stripe_price,
+                interval=self.interval,
+                product=self.product_stripe_id,
+                metadata={
+                    "subscription_plan_price_id": self.id
                 },
                 raw=False
             )
