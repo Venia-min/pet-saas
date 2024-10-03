bfae757342850173d4d3a4ac82d1d5e55f969067
venia-min <57226644+Venia-min@users.noreply.github.com>
2024-10-03 19:17:13 +0200

    Add Scheduled Production Worker Github Action.

--- src/subscriptions/utils.py
+++ src/subscriptions/utils.py
@@ -7,22 +7,22 @@ from subscriptions.models import UserSubscription, Subscription, SubscriptionSta
 def refresh_active_users_subscriptions(
         user_ids=None,
         active_only=True,
-        day_start=0,
-        day_end=0,
-        days_left=0,
-        days_ago=0,
+        day_start=-1,
+        day_end=-1,
+        days_left=-1,
+        days_ago=-1,
         verbose=False):
     qs = UserSubscription.objects.all()
     if active_only:
         qs = qs.by_active_trialing()
     if user_ids is not None:
         qs = qs.by_user_ids(user_ids=user_ids)
-    if days_left > 0:
+    if days_left > -1:
         qs = qs.by_days_left(days_left=days_left)
-    if days_ago > 0:
+    if days_ago > -1:
         qs = qs.by_days_ago(days_ago=days_ago)
-    if day_start > 0 and day_end > 0:
-        qs = qs.by_range(day_start=day_start, day_end=day_end)
+    if day_start > -1 and day_end > -1:
+        qs = qs.by_range(day_start=day_start, day_end=day_end, verbose=verbose)
     complete_count = 0
     qs_count = qs.count()
     for obj in qs:
